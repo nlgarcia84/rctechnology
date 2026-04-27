d2ec0ecbdd10ea57ae516c56478b62ac938b6a29
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-04-27 11:36:37 +0200

    astro config nuevo

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,20 +14,3 @@ export default defineConfig({
     plugins: [],
   },
 });
-
-// @ts-check
-
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
-
-export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  devToolbar: {
-    enabled: false,
-  },
-
-  vite: {
-    plugins: [],
-  },
-});
