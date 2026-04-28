6973d612cc4d090484c90482e27611ada8778071
Norman Leyva Garcia <75862898+nlgarcia84@users.noreply.github.com>
2026-04-28 19:07:22 +0200

    anadido sharp para imagenes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
-// @ts-check
-
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel'; // Asegúrate de que sea '@astrojs/vercel' (serverless) o '@astrojs/vercel/static'
 
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
-  devToolbar: {
-    enabled: false,
-  },
-
-  vite: {
-    plugins: [],
-  },
+  adapter: vercel({
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+      domains: [],
+    },
+    imageService: true, // Esto delega la optimización a Vercel
+  }),
 });
